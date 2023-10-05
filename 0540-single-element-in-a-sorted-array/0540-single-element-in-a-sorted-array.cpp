31a176f7824261658eb044a1029182fbe70ae50a
PRITOM DUTTA <duttapritom777@gmail.com>
2023-10-05 21:32:06 +0530

    Time: 19 ms (62.5%), Space: 22.6 MB (20.01%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& a) {
         int i=0,j=a.size()-1;
-
-        // if(a.size() == 1)
-        // {
-        //     return a[0];
-        // }
-        // if(a.size()==2 && a[0]!=a[0+1])
-        // { 
-        //     return a[0];
-        // }
-
-        while(i<=j)
+        
+        while(i<j)
         {
             int m = (i+j)/2;
 
-            if(m-1>=0 && m+1<a.size())
-            {
-                if(a[m]!=a[m-1] && a[m]!=a[m+1])
-                {
-                    return a[m];
-                }
-                else if((m%2==0 && a[m]==a[m+1]) || (m%2!=0 && a[m]==a[m-1]))
+            // if(m-1>=0 && m+1<a.size())
+            // {
+            //     if(a[m]!=a[m-1] && a[m]!=a[m+1])
+            //     {
+            //         return a[m];
+            //     }
+                 if((m%2==0 && a[m]==a[m+1]) || (m%2!=0 && a[m]==a[m-1]))
                   i=m+1;
                 else
-                  j=m-1;
-            }
-            else
-             break;
+                  j=m;
+            // }
+            // else
+            //  break;
         }
-        if(i==j)
-          return a[i];
-        return a[0];
+        // if(i==j)
+        //   return a[i];
+        // return a[0];
+
+        return a[i];
     }
 };
